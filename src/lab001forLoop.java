80b28a4d9551482f47114bd7b417d0a534b4d3d0
Aditi <aditigawade78@gmail.com>
2024-10-21 22:25:38 +0530

    C:\Users\Aditi\OneDrive\Documents\GitHub\oct-core-java-

---
+++ src/lab001forLoop.java
@@ -0,0 +1,22 @@
+public class lab001forLoop {
+    public static void main(String[]args)
+    {
+        //loop
+        //for loop
+        //repeat something supposed to do method
+        //for(initialization, Conditional,increment ,decrement)
+
+//for (int i=0;i<=10;++i)
+//for(int i=0;i>1;i++)
+for(; ;)
+{
+    System.out.println("hello");
+}
+//condition is important to exit the for loop
+        //long
+        //float
+        //char also we use on it
+
+
+    }
+}
