2bccd1fb3ef4d0250ce51836c443159e393bbee3
Aditi <aditigawade78@gmail.com>
2024-10-25 11:03:46 +0530

    https://github.com/Aditi-oss/oct-core-java-.git

---
+++ src/lab003DowhileLoop.java
@@ -0,0 +1,13 @@
+public class lab003DowhileLoop {
+
+    public static  void main(String[]args)
+    {
+        int a=0;
+        do {
+            System.out.println("print the value "+a);
+            a++;
+            }while (a<10);
+
+        }
+    }
+
