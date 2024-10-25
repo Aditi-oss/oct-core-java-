5db62ecc96ca4b74dd2bb05a44bf242fdfd5d477
Aditi <aditigawade78@gmail.com>
2024-10-25 11:01:05 +0530

    https://github.com/Aditi-oss/oct-core-java-.git

---
+++ src/lab001whileloop.java
@@ -0,0 +1,21 @@
+public class lab001whileloop {
+    public static void main(String[] args) {
+        //loops >repeating block of code
+        //when you are using for statement or while statement  then its verifies the condition befores it executing
+        // the block
+
+        //while A<B<C
+        //I-Intialization
+        //C-condition
+        //U-updation incre/decre
+        //example
+        //A while(c)//block of code// intialization
+        int i=0;
+        while (i<=10)
+        {
+            System.out.println(i);
+            i++;
+            //for and while loop is also called entry controlled loop.
+        }
+    }
+}
---
+++ src/lab002.java
@@ -0,0 +1,11 @@
+public class lab002 {
+    public static  void main(String[]args)
+    {
+        int i=0;
+        while (i<0)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}// the output is nothing printed
